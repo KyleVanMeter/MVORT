4984e0a8a0071faef303367e36698542aa3ca854
Kyle Van Meter <kvanmete76751@gmail.com>
2019-02-02 15:24:27 -0700

    HW1 finished GetMoves()

--- hw1/provided/Maze.cpp
+++ hw1/provided/Maze.cpp
@@ -71,5 +71,33 @@ SquareType Board::get_square_value(Position pos) const {
 
 std::vector<Position> Board::GetMoves(Player *p) {
   std::vector<Position> moves;
-  if(p.get_position().row 
+
+  Position copy = p->get_position();
+
+  bool isUValid = !(get_square_value({copy.row--, copy.col}) == SquareType::Wall
+                   || copy.row-- < 0);
+  bool isDValid = !(get_square_value({copy.row++, copy.col}) == SquareType::Wall
+                   || copy.row++ > BOARDDIM);
+  bool isRValid = !(get_square_value({copy.row, copy.col++}) == SquareType::Wall
+                   || copy.col++ > BOARDDIM);
+  bool isLValid = !(get_square_value({copy.row, copy.col--}) == SquareType::Wall
+                    || copy.col-- < 0);
+
+  if(isUValid) {
+    moves.push_back({copy.row--, copy.col});
+  }
+
+  if(isDValid) {
+    moves.push_back({copy.row++, copy.col});
+  }
+
+  if(isRValid) {
+    moves.push_back({copy.row, copy.col++});
+  }
+
+  if(isLValid) {
+    moves.push_back({copy.row, copy.col--});
+  }
+
+  return moves;
 }
